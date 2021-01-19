6b0b4c900d1780a97adb38c9b29a5379d3dc65e7
HauHe <haukeh@kth.se>
2021-01-19 13:07:49 +0100

    Added function to convert datafile to datapackage.

--- src/data_split.py
+++ src/data_split.py
@@ -2,9 +2,23 @@
 #%% Needed packages
 import sys
 import os
+import subprocess as sp
 import pandas as pd
 import math
-
+from otoole import ReadDatafile
+from otoole import WriteDatapackage
+from otoole import Context
+#%% Convert datafile to datapackage
+def df_to_dp(path):
+    #path = '../data/utopia.txt' #for development
+    #file = '/utopia.txt' #for development
+    dp_path = '../data/datapackage'
+    os.mkdir(dp_path)
+    reader = ReadDatafile()
+    writer = WriteDatapackage()
+    converter = Context(read_strategy=reader, write_strategy=writer)
+    converter.convert(path, dp_path)
+    return dp_path
 #%% Read in csv files from datapackage
 def read_dp(dp_path):
     #dp_path = '../data/datapackage' #for development
@@ -44,29 +58,31 @@ def split_dp(directory,step_size):
             print("Directory is empty")
     else:
         print("Given directory doesn't exist")
-    #%% Derive information on modelling period
-    m_period = pd.read_csv(directory+'/data/YEAR.CSV')
+    # Create datapackage from datafile
+    dp_path = df_to_dp(directory)
+    # Derive information on modelling period
+    m_period = pd.read_csv(dp_path+'/data/YEAR.CSV')
     n_years = len(m_period.index)
     n_steps = n_years/step_size
     full_steps = math.floor(n_steps)
     all_steps = math.ceil(n_steps)
-    #%%
-    dp_dic = read_dp(directory)
+    # Read in datapackage
+    dp_dic = read_dp(dp_path)
     i = 0
     for i in range(all_steps):
         if i+1 < full_steps:
             start = step_size * i
             end = start+(step_size*2)
             step_years = m_period.iloc[start:end]
-            new_dp(dp_dic,step_years,i,directory)
+            new_dp(dp_dic,step_years,i,dp_path)
         else:
             start = i * step_size
             step_years = m_period.iloc[start:]
-            new_dp(dp_dic,step_years,i,directory)
+            new_dp(dp_dic,step_years,i,dp_path)
 #%% data_split executed as script
 if __name__ == '__main__':
     #%% Inputs
-    path = '../data/datapackage' #for developing
+    path = '../data/utopia.txt' #for developing
     step = 5 # for developing
     #path = sys.argv[1]
     #step = sys.argv[2]
