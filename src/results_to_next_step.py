f3ccfa57a7aaeac6554aa8aa80135e3d940447d1
HauHe <haukeh@kth.se>
2021-03-17 19:44:56 +0100

    Added filters to the calculation of the new ResidualCapacity

--- src/results_to_next_step.py
+++ src/results_to_next_step.py
@@ -8,14 +8,22 @@ def main(dp_path,fr_path):
     rc_path = dp_path +'/ResidualCapacity.csv'
     ol_path = dp_path +'/OperationalLife.csv'
     nc_path = fr_path +'/NewCapacity.csv'
+    yr_path = dp_path +'/YEAR.csv'
     df_init = pd.read_csv(rc_path)
     df_ol = pd.read_csv(ol_path)
     df_nc = pd.read_csv(nc_path)
+    df_yr = pd.read_csv(yr_path)
     df_out = df_init
     tec = pd.Series(df_init['TECHNOLOGY'].unique())
-    for 
-    for t in range(len(df_nc)):
-        if #goal: add techs to series that are not in residual capacity but in new capacity
+    tec = tec.append(pd.Series(df_nc['TECHNOLOGY'][~df_nc.TECHNOLOGY.isin(tec)].unique()),ignore_index=True)
+    tec = tec[tec.isin(df_ol['TECHNOLOGY'])]
+    for t in tec:
+        for y in df_yr['VALUE']:
+            df = df_nc
+            df = df[df['TECHNOLOGY']==t]
+            ol = df_ol.loc[df_ol.loc[df_ol['TECHNOLOGY']==t].index[0],'VALUE']
+            df = df[df['YEAR']>(y-ol)]
+            
 
     return
 #%% If run as script
