f8ae9e4032ac395840c56f562fc8eb3046b499fc
HauHe <haukeh@kth.se>
2021-03-18 17:24:44 +0100

    Script now sums residual capacity in next step with the new installed capacities from the final results and writes a new ResidualCapacity.csv file. If called directly the script takes path to the datapackage and path to final results as systems argument inputs

--- src/results_to_next_step.py
+++ src/results_to_next_step.py
@@ -1,6 +1,7 @@
 # This script passes the needed results from one step to the next one
 #%% Import needed packages
 import pandas as pd
+import sys
 #%% Main function to coordinate the script
 def main(dp_path,fr_path):
     dp_path = '../data/datapackage1/data' #for testing
@@ -17,16 +18,24 @@ def main(dp_path,fr_path):
     tec = pd.Series(df_init['TECHNOLOGY'].unique())
     tec = tec.append(pd.Series(df_nc['TECHNOLOGY'][~df_nc.TECHNOLOGY.isin(tec)].unique()),ignore_index=True)
     tec = tec[tec.isin(df_ol['TECHNOLOGY'])]
-    for t in tec:
-        for y in df_yr['VALUE']:
-            df = df_nc
-            df = df[df['TECHNOLOGY']==t]
-            ol = df_ol.loc[df_ol.loc[df_ol['TECHNOLOGY']==t].index[0],'VALUE']
-            df = df[df['YEAR']>(y-ol)]
-            
-
+    for r in df_nc['REGION']:
+        for t in tec:
+            for y in df_yr['VALUE']:
+                df = df_nc
+                df = df[df['TECHNOLOGY']==t]
+                ol = df_ol.loc[df_ol.loc[df_ol['TECHNOLOGY']==t].index[0],'VALUE']
+                df = df[df['YEAR']>(y-ol)]
+                if t in df_init['TECHNOLOGY'].unique():
+                    i = df_out.loc[(df_out['TECHNOLOGY']==t)&(df_out['YEAR']==y)].index[0]
+                    df_out.loc[i,'VALUE'] = df_out.loc[i,'VALUE'] + df['VALUE'].sum()
+                else:
+                    df_out.append(pd.DataFrame([[r,t,df['VALUE'].sum(),y]],columns=['REGION','TECHNOLOGY','VALUE','YEAR']),ignore_index=True)
+    df_out.to_csv(rc_path,index=False)
     return
 #%% If run as script
 if __name__ == '__main__':
-    dp_path = '../data/datapackage1/data' #for testing
-    fr_path = '../results' #for testing
+    #dp_path = '../data/datapackage1/data' #for testing
+    #fr_path = '../results' #for testing
+    dp_path = sys.argv[1]
+    fr_path = sys.argv[2]
+    main(dp_path,fr_path)
