206be8f168f148abb3031882abd0371134af6e9c
HauHe <haukeh@kth.se>
2021-03-25 18:50:49 +0100

    Moved files for multi-stage multi-scenarios runs one folder up.

--- src/main_ms.py
+++ src/main_ms.py
@@ -2,18 +2,24 @@
 #%% Import required packages
 import pandas as pd
 import os
+from .. import data_split
 #%% Function to derive scenario information from provided folders and files
 def get_scen(path):
     path = '../../data/scenarios/' #for testing
     stages = next(os.walk(path))[1]
-    dec_dic = dict()
+    dic = dict() # initialising dictionaries for stages with decisions creating new scenarios
     for s in range(len(stages)):
         path_s = path + stages[s] + '/'
         for root, dirs, files in os.walk(path_s):
-            dec_dic[int(stages[s])] = [f for f in files if not f[0] == '.']
-    return
+            dic[int(stages[s])] = [f for f in files if not f[0] == '.']
+    return dic
 #%% Main function to coordinate the script
-def main():
-
+def main(data_path,step_length,param_path):
+    dic_yr_in_steps, full_steps = 
+    dec_dic = get_scen(param_path)
 #%% If run as script
 if __name__ == '__main__':
+    path_param = '../../data/scenarios/'
+    path_data = '../data/utopia.txt'
+    step_length = 5
+    main(path_data,step_length,path_param)
