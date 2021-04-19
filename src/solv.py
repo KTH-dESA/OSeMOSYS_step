0081272020833e70aa8a57815875cce5f54defff
HauHe <haukeh@kth.se>
2021-04-19 18:32:44 +0200

    solv.py is a script providing the option to solve the models in the step function with other solvers than GLPK. So far it contains functions to create a directory for the results, generate lp files and run the lp file with gurobi generating a sol file. Pending is a function to convert the sol files to csv files and functions to use other solvers than gurobi.

---
+++ src/solv.py
@@ -0,0 +1,16 @@
+"This script conducts the runs of OSeMOSYS models within the OSeMOSYSstep function in cases another solver than glpk is selected"
+#%% Import of needed packages
+#%% Function to create lp file
+def create_lp(path_df):
+    return path_lp
+#%% Function to solve lp file using gurobi
+def sol_gurobi(path_lp):
+    return path_sol
+#%% Function to solve lp file using cbc
+def sol_cbc(path_lp):
+    return path_sol
+#%% Function to solve lp file using cplex
+def sol_cplex(path_lp):
+    return path_sol
+#%% main function
+def main(solver,path_df,path_res):
