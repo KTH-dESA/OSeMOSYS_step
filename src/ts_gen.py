099764386ac99787fce8564044741bc65d3a2cf8
HauHe <haukeh@kth.se>
2021-04-29 19:51:56 +0200

    Initialized a script to create time series for parameter where only a growth rate is indicated.

---
+++ src/ts_gen.py
@@ -0,0 +1,3 @@
+"This script is part of the OSeMOSYS_step function and allows the script to handle inputs for scenarios without the indication of years but a growth rate. The script takes the growth rate and the value of the decision parameter of the last year of the previous step."
+#%% Import of required packages
+import pandas as pd
