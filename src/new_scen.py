2bf244b1bcdbe7661b81259da5c4b1f806e0bc60
HauHe <haukeh@kth.se>
2021-05-03 19:13:41 +0200

    In new_scen.py added check if scen df contains rows with YEAR NaN. And if so call of ts_gen.py. In ts_gen.py started writing function to create time series of based on last value in previous step and indicated growth rate.

--- src/new_scen.py
+++ src/new_scen.py
@@ -2,14 +2,15 @@
 #%% Import needed packages
 import pandas as pd
 import os
+import ts_gen as tg
 import main_ms as mm #for testing
 #%% Main function to coordinate the script
 def main(path_data,step,dic_dec,dic_scen_dec,dic_yrs):
     # path_data = '../data/step2/C0E0/C0' #for testing
     # step = 2 #for testing
     # dic_dec = mm.get_scen('../data/scenarios/')[2] #for testing
-    # dic_scen_dec =  {'C': '0'} #for testing
-    # dic_yrs = {0: pd.DataFrame({'VALUE': [1990,1991]}), 1: pd.DataFrame({'VALUE': [1991,1992,1993,1994,1995,1996,1997,1998,1999,2000]}), 2: pd.DataFrame({'VALUE': [1996,1997,1998,1999,2000,2001,2002,2003,2004,2005]})}
+    # dic_scen_dec =  {'C': '0','E':'0'} #for testing
+    # dic_yrs = {0: pd.DataFrame({'VALUE': [1990,1991]}), 1: pd.DataFrame({'VALUE': [1991,1992,1993,1994,1995,1996,1997,1998,1999,2000]}), 2: pd.DataFrame({'VALUE': [1996,1997,1998,1999,2000,2001,2002,2003,2004,2005]})} #for testing
     for d in dic_dec: # iterate over decisions
         for p in dic_dec[d]['PARAMETER'].unique(): #iterate over parameter in decision
             for dp in range(step,len(dic_yrs)): #iterate over steps
@@ -17,6 +18,8 @@ def main(path_data,step,dic_dec,dic_scen_dec,dic_yrs):
                 df = pd.DataFrame()
                 df = pd.read_csv(path_p)
                 df_in = dic_dec[d][dic_dec[d]['OPTION']==int(dic_scen_dec[d])]
+                if len(df_in['YEAR'][df_in['YEAR'].isnull()])>0:
+                    df_in = tg.main(df_in,dic_yrs[2],path_data,step)
                 df_in = df_in[df_in['YEAR'].isin(dic_yrs[dp]['VALUE'])]
                 if df.empty:
                     df_in = df_in[list(df.columns)]
--- src/ts_gen.py
+++ src/ts_gen.py
@@ -1,3 +1,20 @@
 "This script is part of the OSeMOSYS_step function and allows the script to handle inputs for scenarios without the indication of years but a growth rate. The script takes the growth rate and the value of the decision parameter of the last year of the previous step."
 #%% Import of required packages
 import pandas as pd
+import main_ms as mm #for testing
+#%% Main function
+"The main function receives the dataframe with the parameter information that should go into a datapackage..."
+def main(df,dic_yrs,path_data,step):
+    df = mm.get_scen('../data/scenarios/')[2]['E'] #for testing
+    df = df[df['OPTION']==0]
+    dic_yrs = {0: pd.DataFrame({'VALUE': [1990,1991]}), 1: pd.DataFrame({'VALUE': [1991,1992,1993,1994,1995,1996,1997,1998,1999,2000]}), 2: pd.DataFrame({'VALUE': [1996,1997,1998,1999,2000,2001,2002,2003,2004,2005]})} #for testing
+    path_data = '../data/step2/C0E0/C0' #for testing
+    step = 2 #for testing
+    scens = '/'.join(path_data.split('/')[3:-1])
+    path_data_ps = '../data/step%(step)s/%(scens)s' % {'step': step-1, 'scens': scens}
+    df_w = df[df['YEAR'].isnull()]
+    for p in df_w['PARAMETER'].unique():
+        for t in df_w['TECHNOLOGY'].unique():
+            df_para_ps = pd.read_csv(path_data_ps+'/'+p+'.csv')
+            print(t)
+    return
