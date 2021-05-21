a24773e5c4d985d6d9c1b3a4e7a153813dfe6057
HauHe <haukeh@kth.se>
2021-05-21 14:34:36 +0200

    Corrected the cell call from iloc to loc since the actual index is determined, and not the row number.' q

--- src/ts_gen.py
+++ src/ts_gen.py
@@ -31,10 +31,9 @@ def main(df,dic_yrs,path_data,step,dp):
                 df_para_ps = pd.read_csv(path_data_ps+'/'+p+'.csv')
                 if len(df_para_ps)==0:
                     sys.exit('Seems like you are providng a growth rate for a parameter that has not been defined before. For parameter that have not been defined before, please provide in the first step where the paremter is to be defined a time series.')
-                last_value_ps = df_para_ps.iloc[df_para_ps[df_para_ps['YEAR']==last_yr_ps].index.tolist()[0]]['VALUE']
-                growth = df_w.iloc[df_w[(df_w['PARAMETER']==p)&(df_w[df_w.columns[2]]==t)].index.tolist()[0]]['VALUE']
+                last_value_ps = df_para_ps.loc[df_para_ps[df_para_ps['YEAR']==last_yr_ps].index.tolist()[0]]['VALUE']
+                growth = df_w.loc[df_w[(df_w['PARAMETER']==p)&(df_w[df_w.columns[2]]==t)].index.tolist()[0]]['VALUE']
                 for y in dic_yrs[dp]['VALUE']:
                     value = last_value_ps * (1+growth)**(y-last_yr_ps)
                     df_out = df_out.append(pd.DataFrame([[p,r,t,df_w['OPTION'].unique()[0],y,value]],columns=col),ignore_index=True)
-                    print(value)
     return df_out
