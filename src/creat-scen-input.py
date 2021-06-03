b168aab870b3a6e6b519ebf5ba1863fda9d78cb0
HauHe <haukeh@kth.se>
2021-06-03 12:16:25 +0200

    Added function to read in TECHNOLOGY.csv

--- src/creat-scen-input.py
+++ src/creat-scen-input.py
@@ -1,1 +1,13 @@
+#%%
 import pandas as pd
+#%% Get technologies
+def get_techs(path):
+    df = pd.read_csv(path)
+    return df
+#%% Main function
+def main():
+    tech_path = '../../OSeMBE_dev/input_data/REF/data/TECHNOLOGY.csv'
+    techs = get_techs(tech_path)
+    return techs
+if __name__ == '__main__':
+    tec = main()
