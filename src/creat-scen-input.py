10e3dbe3a13af4b6e8ddfa5699feb325f0b3e64c
HauHe <haukeh@kth.se>
2021-06-03 15:03:10 +0200

    Added function to filter to wanted technology.

--- src/creat-scen-input.py
+++ src/creat-scen-input.py
@@ -4,10 +4,25 @@ import pandas as pd
 def get_techs(path):
     df = pd.read_csv(path)
     return df
+#%% Filter technologies to needed ones
+def filter_tec(df,token):
+    #df = techs #for testing
+    #token = 'CS' #for testing
+    df['TECHNOLOGY'] = df['VALUE'].str.slice(start=4,stop=6)
+    df_new = df[df['TECHNOLOGY'].str.contains(token)]
+    df_new = df_new['VALUE'].reset_index(drop=True)
+    return df_new
+#%%
+
 #%% Main function
-def main():
-    tech_path = '../../OSeMBE_dev/input_data/REF/data/TECHNOLOGY.csv'
-    techs = get_techs(tech_path)
-    return techs
+def main(tec_path,tec_filter):
+    #tec_path = '../../OSeMBE_dev/input_data/REF/data/TECHNOLOGY.csv' #for testing
+    #tec_filter = 'CS' #for testing
+    techs = get_techs(tec_path)
+    techs_selec = filter_tec(techs,tec_filter)
+    return techs_selec
+#%%
 if __name__ == '__main__':
-    tec = main()
+    tech_path = '../../OSeMBE_dev/input_data/REF/data/TECHNOLOGY.csv'
+    tech_filter = 'CS'
+    tec = main(tech_path,tech_filter)
