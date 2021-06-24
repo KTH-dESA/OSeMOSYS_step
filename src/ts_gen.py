3b8fbbcbd0429c5ccc7e397cf30c271448926ea0
HauHe <haukeh@kth.se>
2021-06-24 15:27:59 +0200

    Corrected filtering when reading last value from previous step.

--- src/ts_gen.py
+++ src/ts_gen.py
@@ -3,7 +3,7 @@
 import pandas as pd
 import os
 import sys
-import main_ms as mm #for testing
+# import main_ms as mm #for testing
 #%% Create time series using a growth rate
 def ts_rate(rate,yr,init,yr0):
     value = init * (1+rate)**(yr-yr0)
@@ -41,7 +41,7 @@ def main(df,dic_yrs,path_data,step,dp):
                 df_para_ps = pd.read_csv(os.path.join(path_data_ps,p)+'.csv')
                 if len(df_para_ps[df_para_ps['YEAR']==last_yr_ps])==0:
                     sys.exit('Seems like you are providng a change rate for a parameter that has not been defined before or that is at least not defined in the last year of the previous step. For parameter that have not been defined before, please provide in the first step where the paremter is to be defined a time series.')
-                last_value_ps = df_para_ps.loc[df_para_ps[df_para_ps['YEAR']==last_yr_ps].index.tolist()[0]]['VALUE']
+                last_value_ps = df_para_ps.loc[df_para_ps[(df_para_ps['YEAR']==last_yr_ps)&(df_para_ps.iloc[:,1]==t)].index.tolist()[0]]['VALUE']
                 change = df_w.loc[df_w[(df_w['PARAMETER']==p)&(df_w[df_w.columns[2]]==t)].index.tolist()[0]]['VALUE']
                 if str(change)[-1]=='%': # check if change is a growth rate or an absolute value
                     change = float(change[:-1])/100
